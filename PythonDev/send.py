d3b647a40adbd97d7cc79eb9fbbbba2c0b79589d
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2021-05-30 22:07:30 +0000

    send email edited

--- PythonDev/send.py
+++
@@ -1,6 +0,0 @@
- from formatting import fm
-from datetime import datetime
-
- def send(name):
-     msg = fm()
-     return msg                                                                                                                                                            
